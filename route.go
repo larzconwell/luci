41e3fdd94c420758ef75d96861c9cd5b86cdf19f
Larz Conwell <larzconwell@gmail.com>
2023-11-11 16:01:01 -0500

    Add ability to build route paths

--- route.go
+++ route.go
@@ -1,8 +1,17 @@
 package luci
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"net/http"
+	"net/url"
+	"regexp"
+	"strings"
+)
+
+var (
+	varMatcher = regexp.MustCompile("^{([^:]*):?(.*)}$")
 )
 
 type routeContextKey struct{}
@@ -22,4 +31,96 @@ func RequestRoute(req *http.Request) Route {
 
 func (route Route) String() string {
 	return fmt.Sprintf("%s %s %s", route.Name, route.Method, route.Pattern)
+}
+
+func (route Route) Path(vals ...string) (string, error) {
+	if route.Pattern == "" {
+		return "", errors.New("luci: route pattern must not be empty")
+	}
+	if route.Pattern[0] != '/' {
+		return "", errors.New("luci: route pattern must begin with /")
+	}
+
+	var valIndex int
+	var varCount int
+	var builder strings.Builder
+	pattern := []byte(route.Pattern)[1:]
+	parts := bytes.Split(pattern, []byte{'/'})
+
+	for _, part := range parts {
+		_ = builder.WriteByte('/')
+		var value string
+
+		for len(part) != 0 {
+			start := bytes.IndexAny(part, "{*")
+			if start == -1 {
+				value = string(part)
+				break
+			}
+
+			isWildcard := part[start] == '*'
+			end := start
+
+			var name string
+			var matcherStr string
+			if !isWildcard {
+				end = bytes.IndexByte(part, '}')
+				if end == -1 {
+					return "", errors.New("luci: invalid route pattern")
+				}
+
+				matches := varMatcher.FindStringSubmatch(string(part[start : end+1]))
+				if len(matches) >= 2 {
+					name = matches[1]
+				}
+				if len(matches) == 3 && matches[2] != "" {
+					matcherStr = matches[2]
+				}
+			}
+
+			// If there's no vals left, keep count of vars so an
+			// accurate count can be returned in the final error
+			if len(vals)-1 < valIndex {
+				valIndex++
+				varCount++
+				part = part[end+1:]
+				continue
+			}
+
+			val := vals[valIndex]
+			if !isWildcard && strings.Contains(val, "/") {
+				return "", fmt.Errorf(`luci: value for variable "%s" must not contain /`, name)
+			}
+
+			if matcherStr != "" {
+				matcher, err := regexp.Compile(matcherStr)
+				if err != nil {
+					return "", fmt.Errorf(`luci: variable "%s" must have valid regex: %w`, name, err)
+				}
+
+				if !matcher.MatchString(val) {
+					return "", fmt.Errorf(`luci: value for variable "%s" does not match regex`, name)
+				}
+			}
+
+			value += string(part[:start]) + val
+
+			valIndex++
+			varCount++
+			part = part[end+1:]
+		}
+
+		split := strings.Split(value, "/")
+		for idx, val := range split {
+			split[idx] = url.PathEscape(val)
+		}
+
+		_, _ = builder.WriteString(strings.Join(split, "/"))
+	}
+
+	if varCount != len(vals) {
+		return "", fmt.Errorf("luci: must provide the expected number of values (expected %d received %d)", varCount, len(vals))
+	}
+
+	return builder.String(), nil
 }
--- route_test.go
+++ route_test.go
@@ -2,6 +2,7 @@ package luci
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -34,4 +35,141 @@ func TestRouteString(t *testing.T) {
 	}
 
 	assert.Equal(t, "status GET /status", route.String())
+}
+
+func TestRoutePath(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		route        Route
+		vals         []string
+		expectedPath string
+		expectedErr  error
+	}{
+		{
+			route:        Route{Pattern: "/health"},
+			expectedPath: "/health",
+		},
+		{
+			route:        Route{Pattern: "/static/*"},
+			vals:         []string{"css/index.css"},
+			expectedPath: "/static/css/index.css",
+		},
+		{
+			route:        Route{Pattern: "/media/{}"},
+			vals:         []string{"image_123.jpg"},
+			expectedPath: "/media/image_123.jpg",
+		},
+		{
+			route:        Route{Pattern: "/{resource}"},
+			vals:         []string{"admin"},
+			expectedPath: "/admin",
+		},
+		{
+			route:        Route{Pattern: "/media/random files/{file}"},
+			vals:         []string{"luci 123.jpg"},
+			expectedPath: "/media/random%20files/luci%20123.jpg",
+		},
+		{
+			route:        Route{Pattern: "/user/{user}"},
+			vals:         []string{"luci"},
+			expectedPath: "/user/luci",
+		},
+		{
+			route:        Route{Pattern: "/user/{user}/edit"},
+			vals:         []string{"luci"},
+			expectedPath: "/user/luci/edit",
+		},
+		{
+			route:        Route{Pattern: "/user/{user}/post/{post}"},
+			vals:         []string{"luci", "123"},
+			expectedPath: "/user/luci/post/123",
+		},
+		{
+			route:        Route{Pattern: "/user/{user}/static/*"},
+			vals:         []string{"luci", "js/luci.js"},
+			expectedPath: "/user/luci/static/js/luci.js",
+		},
+		{
+			route:        Route{Pattern: "/user/{user}/post/{post}/static/*"},
+			vals:         []string{"luci", "123", "js/luci.js"},
+			expectedPath: "/user/luci/post/123/static/js/luci.js",
+		},
+		{
+			route:        Route{Pattern: "/search/user/{first}_{last}"},
+			vals:         []string{"luci", "last"},
+			expectedPath: "/search/user/luci_last",
+		},
+		{
+			route:        Route{Pattern: "/search/user/{first}_*"},
+			vals:         []string{"luci", "last"},
+			expectedPath: "/search/user/luci_last",
+		},
+		{
+			route:        Route{Pattern: `/search/date/{date:\d\d\d\d-\d\d-\d\d}`},
+			vals:         []string{"2023-10-28"},
+			expectedPath: "/search/date/2023-10-28",
+		},
+		{
+			route:        Route{Pattern: "/anon/{:[a-z]+[0-9]*}"},
+			vals:         []string{"abc123"},
+			expectedPath: "/anon/abc123",
+		},
+		{
+			expectedErr: errors.New("luci: route pattern must not be empty"),
+		},
+		{
+			route:       Route{Pattern: "pattern"},
+			expectedErr: errors.New("luci: route pattern must begin with /"),
+		},
+		{
+			route:       Route{Pattern: "/user/{user"},
+			vals:        []string{"luci"},
+			expectedErr: errors.New("luci: invalid route pattern"),
+		},
+		{
+			route:       Route{Pattern: "/user/{user}"},
+			expectedErr: errors.New("luci: must provide the expected number of values (expected 1 received 0)"),
+		},
+		{
+			route:       Route{Pattern: "/user/{user}"},
+			vals:        []string{"luci", "last"},
+			expectedErr: errors.New("luci: must provide the expected number of values (expected 1 received 2)"),
+		},
+		{
+			route:       Route{Pattern: "/user/{user}/static/*"},
+			vals:        []string{"luci", "last", "js/luci.js"},
+			expectedErr: errors.New("luci: must provide the expected number of values (expected 2 received 3)"),
+		},
+		{
+			route:       Route{Pattern: "/user/{user}"},
+			vals:        []string{"luci/last"},
+			expectedErr: errors.New(`luci: value for variable "user" must not contain /`),
+		},
+		{
+			route:       Route{Pattern: "/invalid_regex/{:[}"},
+			vals:        []string{"abc"},
+			expectedErr: errors.New("luci: variable \"\" must have valid regex: error parsing regexp: missing closing ]: `[`"),
+		},
+		{
+			route:       Route{Pattern: `/date/{date:\d\d\d\d-\d\d-\d\d}`},
+			vals:        []string{"does_not_match"},
+			expectedErr: errors.New(`luci: value for variable "date" does not match regex`),
+		},
+	}
+
+	for idx, test := range tests {
+		path, err := test.route.Path(test.vals...)
+
+		if test.expectedErr != nil {
+			assert.Error(t, err, idx)
+			if err != nil {
+				assert.Equal(t, test.expectedErr.Error(), err.Error(), idx)
+			}
+		} else {
+			assert.NoError(t, err)
+		}
+
+		assert.Equal(t, test.expectedPath, path, idx)
+	}
 }
